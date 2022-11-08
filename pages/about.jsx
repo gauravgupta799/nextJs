deef67df1d346f4c73faa402c8ec9d911a6690fe
gauravgupta <gaurav.mobilefirst@gmail.com>
2022-11-08 17:50:53 +0530

    file-base-routing

---
+++ pages/about.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const About = () => {
+  return (
+    <div>
+      <h2> Hi viewer, You are on the About Page</h2>
+    </div>
+  )
+}
+
+export default About
--- pages/index.tsx
+++ pages/index.tsx
@@ -1,5 +1,4 @@
 import Head from 'next/head'
-import Image from 'next/image'
 import styles from '../styles/Home.module.css'
 
 export default function Home() {
@@ -16,7 +15,6 @@ export default function Home() {
           Welcome to <a href="https://nextjs.org">Next.js!</a>
         </h1>
 
-       
       </main>
 
       <footer className={styles.footer}>
