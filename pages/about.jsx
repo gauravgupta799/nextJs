fd14b2651a5419c308a53c8e5ee1dce1cbae0113
gauravgupta <gaurav.mobilefirst@gmail.com>
2022-11-10 14:02:14 +0530

    Image Component

--- pages/about.jsx
+++ pages/about.jsx
@@ -1,6 +1,7 @@
 import styles from '../styles/Home.module.css'
 import Navbar from "../Components/Navbar";
 import Styles from "../styles/about.module.css";
+import Image from 'next/image';
 
 const About = () => {
   return (
@@ -8,6 +9,10 @@ const About = () => {
       <Navbar/>
       <div className={Styles.aboutContainer}>
          <h2 className={Styles.heading}> Hi viewer, You are on the About Page</h2>
+      </div>
+      <div style={{textAlign:"center"}}>
+        <Image src="https://images.unsplash.com/photo-1518481852452-9415b262eba4?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop" 
+         width="800" height="670" alt="about"/>
       </div>
     </div>
   )
